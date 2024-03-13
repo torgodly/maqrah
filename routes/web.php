e56bb483a7a70ac666c19956f8804fab8026a56b
Abdullah Alhaj <torgodly@gmail.com>
2024-03-13 16:33:46 +0200

    init filamnet and fix the template in place

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//blog
+Route::get('/blog', function () {
+    return view('blog.index');
+});
+
+Route::get('/blog/{post}', function ($post) {
+    return view('blog.show', ['post' => $post]);
+});
